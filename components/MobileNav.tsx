50c8dcf0b85aa98da9117d9f9ebbb070c349ea25
Utkarsh Gual <123834426+ugjss@users.noreply.github.com>
2024-07-26 12:41:31 +0530

    Update MobileNav.tsx

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -26,7 +26,7 @@ const MobileNav = () => {
         <SheetContent side="left" className="border-none bg-dark-1">
           <Link href="/" className="flex items-center gap-1">
             <Image
-              src="/icons/connectvice.svg"
+              src="/icons/Connectvice.svg"
               width={32}
               height={32}
               alt="ConnectVice logo"
