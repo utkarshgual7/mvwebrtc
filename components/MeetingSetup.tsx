0fc7be7de847f69c16191902b7dc9fd03e0479b1
vyasanuj <vanuj616@gmail.com>
2025-04-15 23:23:36 +0530

    meetingsetup fixed

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -152,6 +152,211 @@
 // export default MeetingSetup;
 
 
+// 'use client';
+// import { useEffect, useState } from 'react';
+// import { useSearchParams } from 'next/navigation';
+// import {
+//   DeviceSettings,
+//   VideoPreview,
+//   useCall,
+//   useCallStateHooks,
+// } from '@stream-io/video-react-sdk';
+
+// import Alert from './Alert';
+// import { Button } from './ui/button';
+
+// const MeetingSetup = ({
+//   setIsSetupComplete,
+// }: {
+//   setIsSetupComplete: (value: boolean) => void;
+// }) => {
+//   const searchParams = useSearchParams();
+//   const userName = searchParams.get('name') || 'Guest';
+//   const { useCallEndedAt, useCallStartsAt } = useCallStateHooks();
+//   const callStartsAt = useCallStartsAt();
+//   const callEndedAt = useCallEndedAt();
+//   const callTimeNotArrived = callStartsAt && new Date(callStartsAt) > new Date();
+//   const callHasEnded = !!callEndedAt;
+//   const [isMicCamToggled, setIsMicCamToggled] = useState(false);
+//   const call = useCall();
+
+//   if (!call) {
+//     throw new Error('useStreamCall must be used within a StreamCall component.');
+//   }
+
+//   useEffect(() => {
+//     if (isMicCamToggled) {
+//       call.camera.disable();
+//       call.microphone.disable();
+//     } else {
+//       call.camera.enable();
+//       call.microphone.enable();
+//     }
+//   }, [isMicCamToggled, call.camera, call.microphone]);
+
+//   if (callTimeNotArrived)
+//     return (
+//       <Alert
+//         title={`Your Meeting has not started yet. It is scheduled for ${callStartsAt.toLocaleString()}`}
+//       />
+//     );
+
+//   if (callHasEnded)
+//     return (
+//       <Alert
+//         title="The call has been ended by the host"
+//         iconUrl="/icons/call-ended.svg"
+//       />
+//     );
+
+//   return (
+//     <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+//       <h1 className="text-center text-2xl font-bold">Setup</h1>
+//       <p className="text-lg">Joining as: {userName}</p>
+//       <VideoPreview />
+//       <div className="flex h-16 items-center justify-center gap-3">
+//         <label className="flex items-center justify-center gap-2 font-medium">
+//           <input
+//             type="checkbox"
+//             checked={isMicCamToggled}
+//             onChange={(e) => setIsMicCamToggled(e.target.checked)}
+//           />
+//           Join with mic and camera off
+//         </label>
+//         <DeviceSettings />
+//       </div>
+//       <Button
+//         className="rounded-md bg-green-500 px-4 py-2.5"
+//         onClick={() => {
+//           call.join({
+//             data: {
+//               custom: {
+//                 name: userName,
+//                 user_id: `user-${Date.now()}`,
+//               }
+//             },
+//           });
+//           setIsSetupComplete(true);
+//         }}
+//       >
+//         Join meeting
+//       </Button>
+//     </div>
+//   );
+// };
+
+// export default MeetingSetup;
+
+
+// 'use client';
+// import { useEffect, useState } from 'react';
+// import { useSearchParams } from 'next/navigation';
+// import {
+//   DeviceSettings,
+//   VideoPreview,
+//   useCall,
+//   useCallStateHooks,
+// } from '@stream-io/video-react-sdk';
+
+// import Alert from './Alert';
+// import { Button } from './ui/button';
+
+// // const MeetingSetup = ({
+// //   setIsSetupComplete,
+// // }: {
+// //   setIsSetupComplete: (value: boolean) => void;
+// // }) => 
+
+// const MeetingSetup = ({
+//   setIsSetupComplete,
+// }: {
+//   setIsSetupComplete: (value: boolean) => void;
+// }) =>
+  
+//   {
+//   const searchParams = useSearchParams();
+//   const userName = searchParams.get('name') || 'Guest';
+//   const { useCallEndedAt, useCallStartsAt } = useCallStateHooks();
+//   const callStartsAt = useCallStartsAt();
+//   const callEndedAt = useCallEndedAt();
+//   const callTimeNotArrived = callStartsAt && new Date(callStartsAt) > new Date();
+//   const callHasEnded = !!callEndedAt;
+//   const [isMicCamToggled, setIsMicCamToggled] = useState(false);
+//   const call = useCall();
+
+//   if (!call) {
+//     throw new Error('useStreamCall must be used within a StreamCall component.');
+//   }
+
+//   useEffect(() => {
+//     if (isMicCamToggled) {
+//       call.camera.disable();
+//       call.microphone.disable();
+//     } else {
+//       call.camera.enable();
+//       call.microphone.enable();
+//     }
+//   }, [isMicCamToggled, call.camera, call.microphone]);
+
+//   if (callTimeNotArrived)
+//     return (
+//       <Alert
+//         title={`Your Meeting has not started yet. It is scheduled for ${callStartsAt.toLocaleString()}`}
+//       />
+//     );
+
+//   if (callHasEnded)
+//     return (
+//       <Alert
+//         title="The call has been ended by the host"
+//         iconUrl="/icons/call-ended.svg"
+//       />
+//     );
+
+//   return (
+//     <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+//       <h1 className="text-center text-2xl font-bold">Setup</h1>
+//       <p className="text-lg">Joining as: {userName}</p>
+//       <VideoPreview />
+//       <div className="flex h-16 items-center justify-center gap-3">
+//         <label className="flex items-center justify-center gap-2 font-medium">
+//           <input
+//             type="checkbox"
+//             checked={isMicCamToggled}
+//             onChange={(e) => setIsMicCamToggled(e.target.checked)}
+//           />
+//           Join with mic and camera off
+//         </label>
+//         <DeviceSettings />
+//       </div>
+//       <Button
+//         className="rounded-md bg-green-500 px-4 py-2.5"
+//         onClick={() => {
+//           call.join({
+//             data: {
+//               members: [{
+//                 role: 'participant',
+//                 user_id: `user-${Date.now()}`,
+//                 custom: {
+//                   name: userName,
+//                 }
+//               }]
+//             }
+//           });
+//           setIsSetupComplete(true);
+//         }}
+//       >
+//         Join meeting
+//       </Button>
+//     </div>
+//   );
+// };
+
+// export default MeetingSetup;
+
+
+
+
 'use client';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
@@ -161,7 +366,6 @@ import {
   useCall,
   useCallStateHooks,
 } from '@stream-io/video-react-sdk';
-
 import Alert from './Alert';
 import { Button } from './ui/button';
 
@@ -228,13 +432,14 @@ const MeetingSetup = ({
       <Button
         className="rounded-md bg-green-500 px-4 py-2.5"
         onClick={() => {
+          const userId = `user-${Date.now()}`;
           call.join({
             data: {
               custom: {
                 name: userName,
-                user_id: `user-${Date.now()}`,
+                user_id: userId
               }
-            },
+            }
           });
           setIsSetupComplete(true);
         }}
